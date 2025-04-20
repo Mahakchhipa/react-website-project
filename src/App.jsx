84c20e334f3ad4e011d00d602cb2f0499c2314a1
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-04-20 10:58:37 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="justify-center text-center text-4xl text-red-600 font-bold ">
+      My Project
+    </div>
+  );
+};
+
+export default App;
