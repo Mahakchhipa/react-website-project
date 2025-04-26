9e342babc621d0a955a10946be7d4375b180eda8
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-04-26 11:07:45 +0530

    Add navbar pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,37 @@
-import React, { useState } from "react";
+import { Router, Routes, Link, Route } from "react-router-dom";
+import Factorial from "./components/Factorial";
+import Sum from "./components/Sum";
+import Mul from "./components/Mul";
 
 const App = () => {
-  const [numbers, setnumbers] = useState({
-    num1: "",
-    num2: "",
-    num3: "",
-  });
   return (
-    <div>
-      <div className="min-h-screen bg-slate-900 flex flex-col justify-center items-center">
-        <h1 className="text-white font-bold text-4xl p-5">
-          Find the Largest Number
-        </h1>
-        <div className="bg-gray-800 p-8 shadow-2xl w-96 border-2 border-blue-500 rounded-lg">
-          <form className="flex flex-col items-center mb-5">
-            <input
-              type="text"
-              name="num1"
-              value={numbers.num1}
-              placeholder="Enter first number"
-              required
-              className="w-full p-3 border border-gray-300 rounded-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
-            />
-            <input
-              type="text"
-              name="num2"
-              placeholder="Enter second number"
-              required
-              value={numbers.num2}
-              className="w-full p-3 border border-gray-300 rounded-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
-            />
-            <input
-              type="text"
-              name="num3"
-              value={numbers.num3}
-              placeholder="Enter third number"
-              required
-              className="w-full p-3 border border-gray-300 rounded-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
-            />
-            <button
-              type="submit"
-              className="bg-green-400 p-3 w-42 rounded-full text-white font-bold text-xl cursor-pointer hover:bg-green-500"
-            >
-              Find Largest
-            </button>
-          </form>
-        </div>
+    <Router>
+      <div>
+        <div className="bg-slate-900 min-h-screen text-white"></div>
+        <nav className="p-6">
+          <ul className="flex justify-center">
+            <li className=" mx-4 text-3xl ">
+              {" "}
+              <Link to="/sum"> Sum </Link>{" "}
+            </li>
+            <li className=" mx-4 text-3xl ">
+              {" "}
+              <Link to="/mul"> Mul </Link>{" "}
+            </li>
+            <li className=" mx-4 text-3xl ">
+              {" "}
+              <Link to="/factorial"> Factorial </Link>{" "}
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/sum" element={<Sum />} />
+          <Route path="/sum" element={<Mul />} />
+          <Route path="/sum" element={<Factorial />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/components/Sum.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Sum = () => {
+  return (
+    <div>
+      <div className="min-h-screen bg-slate-900 flex flex-col justify-center items-center">
+        <h1 className="text-white font-bold text-4xl p-5">
+          Find the Largest Number
+        </h1>
+        <div className="bg-gray-800 p-8 shadow-2xl w-96 border-2 border-blue-500 rounded-lg">
+          <form className="flex flex-col items-center mb-5">
+            <input
+              type="text"
+              name="num1"
+              value={numbers.num1}
+              onChange={handleChange}
+              placeholder="Enter first number"
+              required
+              className="w-full p-3 border border-gray-300 rounded-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
+            />
+            <input
+              type="text"
+              name="num2"
+              placeholder="Enter second number"
+              onChange={handleChange}
+              required
+              value={numbers.num2}
+              className="w-full p-3 border border-gray-300 rounded-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
+            />
+            <input
+              type="text"
+              name="num3"
+              onChange={handleChange}
+              value={numbers.num3}
+              placeholder="Enter third number"
+              required
+              className="w-full p-3 border border-gray-300 rounded-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 text-yellow-400"
+            />
+            <button
+              type="submit"
+              className="bg-green-400 p-3 w-42 rounded-full text-white font-bold text-xl cursor-pointer hover:bg-green-500"
+            >
+              Find Largest
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sum;
